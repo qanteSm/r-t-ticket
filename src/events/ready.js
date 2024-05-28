d88f6b9d7b0dd19ad7c0782c1052d412bc06c899
qryk <qrykra@gmail.com>
2024-05-28 21:49:46 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'ready',	
 	once: true,
 	execute(client) {
-		let activities = [ `Santa Maria`,`R-T Ticket - /help or rt!help`, `Currently serving ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} person!`,`Currently serving ${client.guilds.cache.size} servers!` ], i = 0;
+		let activities = [ `Santa Maria - V1`,`R-T Ticket - /help or rt!help`, `Currently serving ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} person!`,`Currently serving ${client.guilds.cache.size} servers!` ], i = 0;
     setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Playing }), 10000);
 	
 }};
